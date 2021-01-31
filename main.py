a01af5955b39cc8890205fd58bfc7329fa93fd81
Blacky <nicolas.ferbeck@gmail.com>
2021-01-31 18:55:31 +0100

    j ai rajouter les classe

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import pygame
 pygame.init()
 
+# Creez une seconde classe qui va representer notre jeux 
+class Game:
+    def __init__(self):
+        # Generer notre joueur
+        self.player = Player()
+        
+
+# Creez une premiere class qui va representer notre joueur
+class Player(pygame.sprite.Sprite): 
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load('assets/player.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = 400
+        self.rect.y = 500
+    
+
 # Générer la fenêtre de notre jeux
 
 pygame.display.set_caption("Comet fall game")
@@ -10,6 +31,8 @@ screen = pygame.display.set_mode((1080,720))
 # Importer de charger l'arrière plan de notre jeux 
 background = pygame.image.load("assets/bg.jpg")
 
+# Charger notre jeu
+game = Game()
 
 
 running = True
@@ -20,6 +43,9 @@ while running:
 
     # Appliquer l'arrière plan
     screen.blit(background, (0,-200))
+
+    # Appliquer l'image de mon joueur
+    screen.blit(game.player.image , game.player.rect)
 
     # mettre à jour notre écran
     pygame.display.flip()
